84b8b6bb0a0db369026ff16615822b5b9c342afa
Giovanni Cappelletti <tech@methodwolf.com>
2024-07-17 16:03:33 +0200

    update

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,7 +1,33 @@
 <template>
   <h1>Hello World from Home</h1>
+  <ul>
+    <li v-for="farmer in farmers" :key="farmer.id">
+      {{ farmer.name }}
+      {{ farmer.surname }}:
+      {{ farmer.age }}
+    </li>
+  </ul>
 </template>
 
 <script setup>
-console.log('Hello, World!')
+// DIPENDENZE
+import { onMounted, ref } from 'vue'
+import axios from 'axios'
+
+// VARIABILI
+const farmers = ref([])
+
+// EVENTI
+onMounted(() => {
+  axios
+    .get('http://localhost:8080/api/v1/farmer')
+    .then((res) => {
+      console.log(res.data)
+
+      farmers.value = res.data
+    })
+    .catch((err) => {
+      console.log('error!!!!!!!!!')
+    })
+})
 </script>
---
+++ src/views/TestView.vue
@@ -0,0 +1,3 @@
+<template>
+  <h1>Hello from Test</h1>
+</template>
